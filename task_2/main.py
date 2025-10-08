adb0c2f7bac08c79de27fb7ad2caeb9eaa93c48f
rimmachepuxa <ch4epuxa@gmail.com>
2025-10-08 17:01:54 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -1,1 +1,5 @@
-
+# Чикида Римма 
+numbers = input("Enter numbers: ").split() # Вводим числа через пробел 
+list1 = list(map(int,numbers)) # Принимает числа в список 
+list2 = [x**2 for x in list1] #  Возводит числа из первого списка в квадрат 
+print(f"List of squares: {list2}") # Выводит числа в квадрате 
